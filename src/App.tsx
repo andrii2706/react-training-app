f9b6d9fe2eda4989ae65e79d07edc7fd43f8c883
andrii2706 <andriipas200017@gmail.com>
2025-06-03 18:58:52 +0300

    add navigation for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import './App.scss';
 import { Games } from './pages/games/Games.component';
-
+import { NaviagtionBar } from './shared/components/navigation-bar/navigation-bar';
+import { Link, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
+const sideNavItems = (
+  <>
+    <li>
+      <Link to="/characters">Characters</Link>
+    </li>
+    <li>
+      <Link to="/episodes">Developers</Link>
+    </li>
+    <li>
+      <Link to="/locations">Locations</Link>
+    </li>
+    <li>
+      <Link to="/my-watch-list">My Watch list</Link>
+    </li>
+  </>
+)
   return (
-    <div>
-        <Games/>
-    </div>
+    <BrowserRouter>
+      <NaviagtionBar routes={sideNavItems} >
+       <Routes>
+        
+       </Routes>
+      </NaviagtionBar>   
+    </BrowserRouter>
   );
 }
 
