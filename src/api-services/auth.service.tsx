8ddee246fe165eeb715f65b42ea19d3cb2d03a48
andrii2706 <andriipas200017@gmail.com>
2025-07-01 00:53:17 +0300

    create first api call with quth

--- src/api-services/auth.service.tsx
+++ src/api-services/auth.service.tsx
@@ -1,1 +1,27 @@
-export const AuthService = () => {};
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { AuthApp } from "../firebase";
+
+
+export const AuthService = () => {
+  const auth = AuthApp
+  const googleProvider = new GoogleAuthProvider()
+   
+   
+   async function signInWithCredentials(){
+
+    }
+  async  function signOutUser(){
+
+    }
+  async  function registerUser() {
+        
+    }
+  async  function signIsWithGoogle(){
+    try{
+      await signInWithPopup(auth, googleProvider)
+    }catch(error){
+      console.log(error)
+    }
+
+    }
+};
