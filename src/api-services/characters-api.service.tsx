165d0a1fda1254bb0ae777c28efd5019d2c71f9b
andrii2706 <andriipas200017@gmail.com>
2025-07-22 11:39:57 +0300

    add api request

--- src/api-services/characters-api.service.tsx
+++ src/api-services/characters-api.service.tsx
@@ -1,12 +1,11 @@
 const apiRaw = 'https://rickandmortyapi.com/api/characters';
 
-
-export function getAllCharacters(page: number){
-  return fetch(`${apiRaw}/?page=${page}`)
+export function getAllCharacters(page: number) {
+  return fetch(`${apiRaw}/?page=${page}`);
 }
-export function getCharacter(id: number){
-  return fetch(`${apiRaw}/${id}`)
+export function getCharacter(id: number) {
+  return fetch(`${apiRaw}/${id}`);
 }
-export function searchCharacters(options: any){
-  return fetch(`${apiRaw}`, options)
+export function searchCharacters(options: any) {
+  return fetch(`${apiRaw}`, options);
 }
