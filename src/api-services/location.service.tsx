7b54b36320caf02e589d3350534577815de44401
andrii2706 <andriipas200017@gmail.com>
2025-08-03 17:04:22 +0300

    location changes

---
+++ src/api-services/location.service.tsx
@@ -0,0 +1,9 @@
+const apiRaw = 'https://rickandmortyapi.com/api';
+
+export const getLocations = () => {
+    return fetch(`${apiRaw}/location`).then(response => response.json());
+}
+
+export const getLocation = (id: number) => {
+    return fetch(`${apiRaw}/location/${id}`).then(response => response.json());
+}
