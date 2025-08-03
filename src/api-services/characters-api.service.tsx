16095f291d5e0e11864c318c5ad8d9a75f924a85
andrii2706 <andriipas200017@gmail.com>
2025-08-03 16:16:16 +0300

    changes for home page

--- src/api-services/characters-api.service.tsx
+++ src/api-services/characters-api.service.tsx
@@ -4,6 +4,6 @@ export function getCharactersFromBe() {
   return fetch(`${apiRaw}/character`).then(response => response.json());
 }
 
-export const getCharaterFromBe = (id: number) => {
-  return fetch(`${apiRaw}/character/${id}`).then(response => response.json());
-};
+// export const getCharaterFromBe = (id: number) => {
+//   return fetch(`${apiRaw}/character/${id}`).then(response => response.json());
+// };
