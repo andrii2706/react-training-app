ea9a7de92d0edd1f2aefdac667d1211e73646a37
andrii2706 <andriipas200017@gmail.com>
2025-08-03 17:04:49 +0300

    add changes for locations

--- src/api-services/location.service.tsx
+++ src/api-services/location.service.tsx
@@ -1,9 +1,9 @@
 const apiRaw = 'https://rickandmortyapi.com/api';
 
 export const getLocations = () => {
-    return fetch(`${apiRaw}/location`).then(response => response.json());
-}
+  return fetch(`${apiRaw}/location`).then(response => response.json());
+};
 
 export const getLocation = (id: number) => {
-    return fetch(`${apiRaw}/location/${id}`).then(response => response.json());
-}
+  return fetch(`${apiRaw}/location/${id}`).then(response => response.json());
+};
