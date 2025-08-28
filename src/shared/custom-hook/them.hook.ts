5b860960385530a7dbd2b6b108bf6915fe5aac0d
andrii2706 <andriipas200017@gmail.com>
2025-08-28 23:34:35 +0300

    prettier format

--- src/shared/custom-hook/them.hook.ts
+++ src/shared/custom-hook/them.hook.ts
@@ -1,16 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export const useTheme = () => {
   const getSystemTheme = () =>
-    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
-  const [theme, setTheme] = useState<string>(
-    localStorage.getItem("theme") || getSystemTheme()
-  );
+  const [theme, setTheme] = useState<string>(localStorage.getItem('theme') || getSystemTheme());
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   return { theme, setTheme };
