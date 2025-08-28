477fc284e03d4ff4913eca03a8a2e2dada60cdf8
andrii2706 <andriipas200017@gmail.com>
2025-08-28 23:34:14 +0300

    add theme for projects

---
+++ src/shared/custom-hook/them.hook.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useTheme = () => {
+  const getSystemTheme = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+  const [theme, setTheme] = useState<string>(
+    localStorage.getItem("theme") || getSystemTheme()
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return { theme, setTheme };
+};
